5a2a60bda2e1763826d677baddcd63ea29ef2dd0
logonrm <vitorluizc@outlook.com>
2017-05-10 21:24:58 -0300

    Add /test page

--- src/router.js
+++ src/router.js
@@ -31,6 +31,10 @@ const router = new Router({
       meta: {
         auth: true
       }
+    },
+    {
+      path: '/test',
+      component: views.Test
     }
   ]
 })
---
+++ src/views/Test.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  view-container
+    property-form
+</template>
+
+<script>
+  import ViewContainer from '../components/view/Container.vue'
+  import PropertyForm from '../components/property/Form.vue'
+  import * as types from '../store/types.js'
+
+  export default {
+    components: { ViewContainer, PropertyForm }
+  }
+</script>
