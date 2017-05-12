cfd1916ed1e76e3d5f29130a31985575cbab58b3
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2017-05-12 18:38:09 -0300

    Add property form + property store --partial

--- src/components/property/Form.vue
+++ src/components/property/Form.vue
@@ -1,22 +1,42 @@
 <template lang="pug">
   form(@submit.prevent='save')
-    v-select(
-      v-model='selectedState',
-      :items='states',
-      item-value='id',
-      item-text='name',
-      label='Selecione o estado',
-      autocomplete
-    )
-    v-select(
-      v-model='selectedCity',
-      :items='cities',
-      :disabled='cityDisabled',
-      item-value='id',
-      item-text='name',
-      label='Selecione a cidade',
-      autocomplete
-    )
+    v-subheader Endereço
+    v-container(fluid)
+      v-row
+        v-col(xs12, md4)
+          v-select(
+            v-model='state',
+            :items='states',
+            item-value='id',
+            item-text='name',
+            label='Selecione o estado',
+            autocomplete
+          )
+        v-col(xs12, md4)
+          v-select(
+            v-model='city',
+            :items='cities',
+            :disabled='cityDisabled',
+            item-value='id',
+            item-text='name',
+            label='Selecione a cidade',
+            autocomplete
+          )
+        v-col(xs12, md5)
+          v-text-field(
+            v-model='address',
+            label='Logradouro'
+          )
+        v-col(xs3, md2)
+          v-text-field(
+            v-model='number',
+            label='Número'
+          )
+        v-col(xs9, md4)
+          v-text-field(
+            v-model='complement',
+            label='Complemento'
+          )
 </template>
 
 <script>
@@ -25,10 +45,12 @@
   export default {
     data() {
       return {
-        selectedState: null,
-        selectedCity: null,
+        address: null,
+        number: null,
+        city: null,
         cityDisabled: true,
         cities: [],
+        state: null,
         states: [
           {
             id: '-Kjodg1kcPifWWebPf3o',
@@ -38,15 +60,12 @@
       }
     },
     watch: {
-      async selectedState() {
+      async state() {
         this.cityDisabled = true
         this.cities = await this.$store.dispatch(
           types.CITIES_SELECT,
-          this.selectedState.id
+          this.state.id
         )
-
-        console.log(JSON.stringify(this.cities))
-
         this.cityDisabled = false
       }
     }
